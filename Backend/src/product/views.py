f317f220cc6413273d649dae582fab6440967b00
amaan2398 <amaan.nassar@gmail.com>
2020-08-10 16:44:33 +0530

    test

--- Backend/src/product/views.py
+++ Backend/src/product/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    data = [{'name':'pant','price':210.5},{'name':'pant1','price':211.0}]
+    data = [{'name':'pant','price':210.5},{'name':'pant1','price':211.012}]
     return render(request,"index.html",{'data' : data})
