bf7f39f54dfb6a12c5a0d9fd9a26131a8bfe6a2a
Outsideramaan <amaan.nassar@gmail.com>
2020-08-10 16:31:05 +0530

    created Django project, added product app in Django project.

---
+++ Backend/src/product/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    data = [{'name':'pant','price':210.5},{'name':'pant1','price':211.5}]
+    return render(request,"index.html",{'data' : data})
